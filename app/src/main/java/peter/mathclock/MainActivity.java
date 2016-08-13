21abebf170eff8380abf9b4195f105ef6e0e8a8a
Peter <fodpeter@gmail.com>
2016-08-13 13:15:44 +0200

    remove generated FloatingActionButton

--- app/src/main/java/peter/mathclock/MainActivity.java
+++ app/src/main/java/peter/mathclock/MainActivity.java
@@ -4,14 +4,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.IdRes;
 import android.support.annotation.UiThread;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -32,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView clockText;
     private UpdateTask updateTask;
     private Handler handler;
-    private int state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +36,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         ListMultimap<Integer, Integer> mapping = loadMapping();
 
